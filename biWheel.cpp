48cab6654e8fd75b59fbf3391298c8826e9d990f
dubkov <dubkov@users.noreply.github.com>
2014-04-24 15:38:54 +0700

    Update biWheel.cpp

--- biWheel.cpp
+++ biWheel.cpp
@@ -1,4 +1,5 @@
-#include "Arduino.h"
+#include "stdlib.h"
+//#include "Arduino.h"
 #include "biWheel.h"
 
 biWheel::biWheel(int in1, int in2, int in3, int in4)
@@ -42,3 +43,45 @@ void biWheel::rightMotorStop(){
   digitalWrite(_in3,LOW);
   digitalWrite(_in4,LOW);
 }
+
+
+
+
+
+
+void biWheel::leftMotorForwardPWM(int spdl){
+	_spdl = map(spdl,0,100,150,255);
+  digitalWrite(_in2,LOW);
+  analogWrite(_in1,_spdl);
+}
+
+void biWheel::leftMotorBackwardPWM(int spdl){
+	_spdl=map(spdl,0,100,150,255);
+  digitalWrite(_in1,HIGH);
+  analogWrite(_in2,_spdl);
+}
+
+void biWheel::rightMotorForwardPWM(int spdr){
+	_spdr=map(spdr,0,100,150,255);
+digitalWrite(_in4,LOW);
+  analogWrite(_in3,_spdr);
+}
+
+void biWheel::rightMotorBackwardPWM(int spdr){
+	_spdr=map(spdr,0,100,150,255);
+  digitalWrite(_in3,HIGH);
+  analogWrite(_in4,_spdr);
+}
+
+void biWheel::leftMotor(int spdl){
+	if ( spdl == 0 ){ leftMotorStop(); }
+	else if ( spdl > 0 ){ leftMotorForwardPWM(abs(spdl)); }
+	else { leftMotorBackwardPWM(spdl); }
+}
+
+void biWheel::rightMotor(int spdr){
+	if ( spdr == 0 ){ rightMotorStop(); }
+	else if ( spdr > 0 ){ rightMotorForwardPWM(abs(spdr)); }
+	else { rightMotorBackwardPWM(spdr); }
+}
+
