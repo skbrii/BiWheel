51e893814398879da08ef743b1e44572b666db0a
dubkov <dubkov@skbrii.ru>
2014-04-24 23:29:05 +0700

    Cosmetic
    Tabulation etc

--- biWheel.cpp
+++ biWheel.cpp
@@ -1,5 +1,4 @@
 #include "stdlib.h"
-//#include "Arduino.h"
 #include "biWheel.h"
 
 #define LM 0
@@ -7,44 +6,44 @@
 
 biWheel::biWheel(int in1, int in2, int in3, int in4)
 {
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-  pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);
-  _in1 = in1;
-  _in2 = in2;
-  _in3 = in3;
-  _in4 = in4;
+	pinMode(in1, OUTPUT);
+	pinMode(in2, OUTPUT);
+	pinMode(in3, OUTPUT);
+	pinMode(in4, OUTPUT);
+	_in1 = in1;
+	_in2 = in2;
+	_in3 = in3;
+	_in4 = in4;
 }
 
 void biWheel::leftMotorForward(){
-  digitalWrite(_in2,LOW);
-  digitalWrite(_in1,HIGH);
+	digitalWrite(_in2,LOW);
+	digitalWrite(_in1,HIGH);
 }
 
 void biWheel::leftMotorBackward(){
-  digitalWrite(_in1,LOW);
-  digitalWrite(_in2,HIGH);
+	digitalWrite(_in1,LOW);
+	digitalWrite(_in2,HIGH);
 }
 
 void biWheel::leftMotorStop(){
-  digitalWrite(_in1,LOW);
-  digitalWrite(_in2,LOW);
+	digitalWrite(_in1,LOW);
+	digitalWrite(_in2,LOW);
 }
 
 void biWheel::rightMotorForward(){
-  digitalWrite(_in4,LOW);
-  digitalWrite(_in3,HIGH);
+	digitalWrite(_in4,LOW);
+	digitalWrite(_in3,HIGH);
 }
 
 void biWheel::rightMotorBackward(){
-  digitalWrite(_in3,LOW);
-  digitalWrite(_in4,HIGH);
+	digitalWrite(_in3,LOW);
+	digitalWrite(_in4,HIGH);
 }
 
 void biWheel::rightMotorStop(){
-  digitalWrite(_in3,LOW);
-  digitalWrite(_in4,LOW);
+	digitalWrite(_in3,LOW);
+	digitalWrite(_in4,LOW);
 }
 
 
@@ -54,26 +53,26 @@ void biWheel::rightMotorStop(){
 
 void biWheel::leftMotorForwardPWM(int spdl){
 	_spdl = map(spdl,0,100,150,255);
-  digitalWrite(_in2,LOW);
-  analogWrite(_in1,_spdl);
+	digitalWrite(_in2,LOW);
+	analogWrite(_in1,_spdl);
 }
 
 void biWheel::leftMotorBackwardPWM(int spdl){
 	_spdl=map(spdl,0,100,150,255);
-  digitalWrite(_in1,LOW);
-  analogWrite(_in2,_spdl);
+	digitalWrite(_in1,LOW);
+	analogWrite(_in2,_spdl);
 }
 
 void biWheel::rightMotorForwardPWM(int spdr){
 	_spdr=map(spdr,0,100,150,255);
-digitalWrite(_in4,LOW);
-  analogWrite(_in3,_spdr);
+	digitalWrite(_in4,LOW);
+	analogWrite(_in3,_spdr);
 }
 
 void biWheel::rightMotorBackwardPWM(int spdr){
 	_spdr=map(spdr,0,100,150,255);
-  digitalWrite(_in3,LOW);
-  analogWrite(_in4,_spdr);
+	digitalWrite(_in3,LOW);
+	analogWrite(_in4,_spdr);
 }
 
 void biWheel::leftMotor(int spdl){
--- biWheel.h
+++ biWheel.h
@@ -9,31 +9,31 @@
 
 class biWheel
 {
-  public:
-    biWheel(int in1, int in2, int in3, int in4);
-    void leftMotorForward();
-    void leftMotorBackward();
-    void leftMotorStop();
-    void rightMotorForward();
-    void rightMotorBackward();
-    void rightMotorStop();
-    void leftMotorForwardPWM(int spdl);
-    void leftMotorBackwardPWM(int spdl);
-    void rightMotorForwardPWM(int spdr);
-    void rightMotorBackwardPWM(int spdr);
-void drive(boolean mtr, int spd);
-void leftMotor(int spdl);
-void rightMotor(int spdr);
+	public:
+		biWheel(int in1, int in2, int in3, int in4);
+		void leftMotorForward();
+		void leftMotorBackward();
+		void leftMotorStop();
+		void rightMotorForward();
+		void rightMotorBackward();
+		void rightMotorStop();
+		void leftMotorForwardPWM(int spdl);
+		void leftMotorBackwardPWM(int spdl);
+		void rightMotorForwardPWM(int spdr);
+		void rightMotorBackwardPWM(int spdr);
+		void drive(boolean mtr, int spd);
+		void leftMotor(int spdl);
+		void rightMotor(int spdr);
     
-  private:
-    int _in1;
-    int _in2;
-    int _in3;
-    int _in4;
-int _spdl;
-int _spdr;
-int _spd;
-boolean _mtr;
+	private:
+    		int _in1;
+    		int _in2;
+    		int _in3;
+    		int _in4;
+		int _spdl;
+		int _spdr;
+		int _spd;
+		boolean _mtr;
 
 };
 
