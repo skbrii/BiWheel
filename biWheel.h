03c5fd10e93b4d133257c16195f95ef09f803c85
dubkov <dubkov@users.noreply.github.com>
2014-04-24 15:38:20 +0700

    Update biWheel.h

--- biWheel.h
+++ biWheel.h
@@ -1,6 +1,8 @@
 #ifndef biWheel_h
 #define biWheel_h
 
+#include "Arduino.h"
+
 class biWheel
 {
   public:
@@ -11,12 +13,21 @@ class biWheel
     void rightMotorForward();
     void rightMotorBackward();
     void rightMotorStop();
+    void leftMotorForwardPWM(int spdl);
+    void leftMotorBackwardPWM(int spdl);
+    void rightMotorForwardPWM(int spdr);
+    void rightMotorBackwardPWM(int spdr);
+void leftMotor(int spdl);
+void rightMotor(int spdr);
     
   private:
     int _in1;
     int _in2;
     int _in3;
     int _in4;
-}
+int _spdl;
+int _spdr;
+
+};
 
 #endif
