e71356b54557b4185942dee58d8ce0e2b05471c7
dubkov <dubkov@skbrii.ru>
2014-04-28 00:50:36 +0700

    added description

--- biWheel.cpp
+++ biWheel.cpp
@@ -1,3 +1,23 @@
+/* biWheel.cpp
+biWheel is Arduino library for chassis with two motors driven by l293d or l298d H-bridge.  
+
+Created by Ilya S. Dubkov, April 24, 2014
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+*/
+
+
+
+
+
 #include "stdlib.h"
 #include "biWheel.h"
 
