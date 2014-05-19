6fc9b2d537d18d9f400203f46b2d0e2bd778ff36
Илья Дубков <dubkov@users.noreply.github.com>
2014-05-19 22:05:54 +0700

    Update BiWheel.h

--- BiWheel.h
+++ BiWheel.h
@@ -23,10 +23,10 @@ Lesser General Public License for more details.
 #define RM 1
 
 
-class biWheel4
+class biWheel
 {
 	public:
-		biWheel4(int in1, int in2, int in3, int in4);
+		biWheel(int in1, int in2, int in3, int in4);
 		void leftMotorForward();
 		void leftMotorBackward();
 		void leftMotorStop();
@@ -49,38 +49,6 @@ class biWheel4
 		int _spdl;
 		int _spdr;
 		int _spd;
-		boolean _mtr;
-
-};
-
-class biWheel6
-{
-	public:
-		biWheel6(int in1, int in2, int in3, int in4, int ena, int enb);
-		void leftMotorForward();
-		void leftMotorBackward();
-		void leftMotorStop();
-		void rightMotorForward();
-		void rightMotorBackward();
-		void rightMotorStop();
-		void leftMotorForwardPWM(int spdl);
-		void leftMotorBackwardPWM(int spdl);
-		void rightMotorForwardPWM(int spdr);
-		void rightMotorBackwardPWM(int spdr);
-		void drive(boolean mtr, int spd);
-		void leftMotor(int spdl);
-		void rightMotor(int spdr);
-    
-	private:
-    		int _in1;
-    		int _in2;
-    		int _in3;
-    		int _in4;
-		int _spdl;
-		int _spdr;
-		int _spd;
-		int _ena;
-		int _enb;
 		boolean _mtr;
 
 };
